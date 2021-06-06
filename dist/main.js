24b06824f1c99d9d681d99c956e7cd48131dd175
unknown <mosa-rko@windowslive.com>
2021-06-06 14:44:00 +0300

    comment logic added

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,106 @@
-const PostBtn = $('#PostButton');
+const { text } = require("express");
+
+const PostBtn = $('#postButton');
 const inputTextElement = $('input');
 const woofer = new WooferManger();
 const render = new Renderer();
-var counter_user=0;
+const ErrMsg = "";
+const userLogged = false
 const LoadPage = async function () {
-    await Woofer.getPostFromDb();
-    //render.render_data(woofer.UserPosts)
+    if (userLogged === false) {
+        ErrMsg = "you Must Login First"
+        // render.renderError(woofer.UserPosts)
+        return
+    }
+    await woofer.getPostFromDb();
+    // render.renderPosts(woofer.UserPosts)
+    console.log(woofer.UsersPosts)
+}
 
+const LogIn = async function(){
+    var email = prompt("What is your Email?");// this is for testing 
+    userLogged=!userLogged
+    
 }
 
 const savePost = async function () {
-    counter_user++;
+    var username = prompt("What is your name?");// this is for testing 
     const text = inputTextElement.val();
-    if (text === "") { return }
-    const post={
-        user:"user"+counter_user,
-        text:text,
-        likes:[],
-        date: new Date()
+    if (validateInput(text)) {
+
+        const post = {
+            user: username,
+            text: text,
+            likes: [],
+            date: new Date()
+        }
+        woofer.savePostInDB(post)
+    } else {
+        ErrMsg = "input is not valid";
+        //render.render_date({error:ErrMsg})
+        return;
     }
-    woofer.savePost(post)
-        // render.render_data(woofer.UserPosts)
+
+
+    // render.render_data(woofer.UserPosts)
 
 }
-// PostBtn.on("click", savePost){
+const deletePost = async function () {
+    var postId = prompt("What is your deletepodt id?");
+    woofer.deletePostFromDB(postId)
+    // render.render_data(woofer.UserPosts)
 
-// }
+}
+
+const saveComment = async function () {
+    var myPostId = prompt("What is your deletepodt id?");
+    const testcomment1 = {
+        user: "comment1",
+        PostId: myPostId,
+        date: date,
+        text: text
+    }
+    woofer.saveCommentInDB(newTesterComment);
+    // render.render_data(woofer.UserPosts)
+
+
+}
+
+
+
+
+
+
+$('body').on("click", "deleteComment", function () {
+
+})
+
+PostBtn.on("click", savePost)
+
+LoadPage()
+
+deletePost()
+//// helper function 
+function validateInput(input) {
+    if (input === "") { return true }
+    return false
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+
+}
+
+
+
+
+
+
+
+////
+
+
+
+
+
